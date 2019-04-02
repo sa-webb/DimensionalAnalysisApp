3700bbe4e3ecf2d80b77360d25e9ed1eeb1904ea
Austin Webb <stevenaustinwebb@gmail.com>
2019-04-02 19:43:30 -0400

    added elements lib

--- components/Submit.js
+++ components/Submit.js
@@ -1,15 +1,54 @@
 import React, { Component } from 'react';
 import { FlatList, StyleSheet, Text, Image, View, TextInput, TouchableHighlight } from 'react-native';
+import { ThemeProvider, Button } from 'react-native-elements';
 
-
-export default class Submit extends Component {
-    render() {
-        return (
-        <TouchableHighlight onPress={() => this.props.navigation.navigate('Home') }>
+/**
+ *  <TouchableHighlight onPress={() => this.props.navigation.navigate('Home') }>
             <View style={styles.submitButton}>
                 <Text style={styles.Text}>Submit</Text>
             </View>
         </TouchableHighlight>
+ */
+
+const theme = {
+    Button: {
+      titleStyle: {
+        color: 'white',
+        fontSize: 32,
+        width: 250,
+        height: 43,
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
+    },
+    colors: {
+        primary: 'blue',
+        secondary: 'grey',
+      },
+  };
+export default class Submit extends Component {
+    render() {
+        return (
+            <ThemeProvider theme={theme}>
+                <Button title="Submit" />
+            </ThemeProvider>
         )
     }
 }
+
+// const styles = StyleSheet.create({
+//     submitButton: {
+//         width: 250,
+//         height: 50,
+//         marginTop: 20,
+//         alignItems: 'center',
+//         justifyContent: 'center',
+//         backgroundColor: 'blue',
+//         borderColor: 'black',
+//       },
+//       Text: {
+//         fontSize: 20,
+//         color: 'white',
+//         textAlign: 'center'
+//       }
+// })
